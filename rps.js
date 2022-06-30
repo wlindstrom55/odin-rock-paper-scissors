4b107d00552bad7abba5e782c1df5a8c7e37cf40
wlindstrom55 <75394313+wlindstrom55@users.noreply.github.com>
2022-06-29 23:28:56 -0500

    finished basic UI w/ button event handlers and readout of results, game ends at first to 5

--- rps.js
+++ rps.js
@@ -1,4 +1,4 @@
-//first function should randomly return either 'Rock' 'Paper' or 'Scissors'.
+//first function should randomly return either 'Rock' 'Paper' or 'Scissors'. This will be the 'opponents' turn
 function computerPlay() {
     let choice = Math.random() * 10;
     //console.log(choice);
@@ -19,69 +19,51 @@ function computerPlay() {
     return ret;
 }
 
-//second function plays a single round of rock-paper-scissors. Should take 2 param, and return a string that declares the winner.
+//second function is logic for a single round of rock-paper-scissors.
 function play(cleanPrompt, computerSelection) {
     let cleanPS = cleanPrompt;
     let retVal;
     let didIWin;
-
-    //const container = document.querySelector('.results');
-
-if (cleanPS == 'Rock' && computerSelection == 'Rock') {
-    retVal = `There was a tie! You and the opponent both chose ${cleanPS}`;
-    didIWin = 'Tie';
-    
-    // const contentRockTie = document.createElement('div');
-    // contentRockTie.classList.add('contentRockTie');
-    // contentRockTie.textContent = retVal;
-    // console.log("test");
-}
-if (cleanPS == 'Rock' && computerSelection == 'Paper') {
-    retVal = 'You lose! Paper beats rock.';
-    didIWin = 'Loss';
-
-    // const contentRockLoss = document.createElement('div');
-    // contentRockLoss.classList.add('contentRockLoss');
-    // contentRockLoss.textContent = retVal;
-    // console.log("test");
-}
-if (cleanPS == 'Rock' && computerSelection == 'Scissors') {
-    retVal = 'You win! Rock crushes scissors.';
-    didIWin = 'Win';
-
-    // const contentRockWin = document.createElement('div');
-    // contentRockWin.classList.add('contentRockWin');
-    // contentRockWin.textContent = retVal;
-    // console.log("test");
-}
-if (cleanPS == 'Paper' && computerSelection == 'Rock') {
-    retVal = 'You win! Paper suffocates rock.';
-    didIWin = 'Win';
-}
-if (cleanPS == 'Paper' && computerSelection == 'Paper') {
-    retVal = 'You tied! Paper cancels out.';
-    didIWin = 'Tie';
-}
-if (cleanPS == 'Paper' && computerSelection == 'Scissors') {
-    retVal = 'You lose. Your paper was shredded by scissors.';
-    didIWin = 'Loss';
-}
-if (cleanPS == 'Scissors' && computerSelection == 'Paper') {
-    retVal = 'You win handily, as your scissors mercilessly cut up the enemy paper.';
-    didIWin = 'Win';
-}
-if (cleanPS == 'Scissors' && computerSelection == 'Rock') {
-    retVal = 'You lose. Scissor chips against rock.';
-    didIWin = 'Loss';
-}
-if (cleanPS == 'Scissors' && computerSelection == 'Scissors') {
-    retVal = 'You tie. Scissors clash against each other and cancel out.';
-    didIWin = 'Tie';
-}  
-return [retVal , didIWin];
+    if (cleanPS == 'Rock' && computerSelection == 'Rock') {
+        retVal = `There was a tie! Rock cancels out.`;
+        didIWin = 'Tie';
+    }
+    if (cleanPS == 'Rock' && computerSelection == 'Paper') {
+        retVal = 'You lose! Rock is enveloped and swallowed alive mercilessly by paper.';
+        didIWin = 'Lose';
+    }
+    if (cleanPS == 'Rock' && computerSelection == 'Scissors') {
+        retVal = 'You win! Rock shatters scissors or maybe scissors just cannot fit around rock.';
+        didIWin = 'Win';
+    }
+    if (cleanPS == 'Paper' && computerSelection == 'Rock') {
+        retVal = 'You win! Paper wraps around rock?';
+        didIWin = 'Win';
+    }
+    if (cleanPS == 'Paper' && computerSelection == 'Paper') {
+        retVal = 'You tied! Paper cancels out.';
+        didIWin = 'Tie';
+    }
+    if (cleanPS == 'Paper' && computerSelection == 'Scissors') {
+        retVal = 'You lose. Your paper was shredded by scissors.';
+        didIWin = 'Lose';
+    }
+    if (cleanPS == 'Scissors' && computerSelection == 'Paper') {
+        retVal = 'You win handily, as your scissors mercilessly cut up the enemy paper.';
+        didIWin = 'Win';
+    }
+    if (cleanPS == 'Scissors' && computerSelection == 'Rock') {
+        retVal = 'You lose. Scissors are weak against rock.';
+        didIWin = 'Lose';
+    }
+    if (cleanPS == 'Scissors' && computerSelection == 'Scissors') {
+        retVal = 'You tie. Scissors clash against each other and cancel out.';
+        didIWin = 'Tie';
+    }  
+    return [retVal , didIWin];
 }
 
-//3rd function will be for the utility of cleaning up a player's input
+//3rd function will be for the utility of cleaning up a given input
 function cleanPrompt(input) {
     let a = input.trim().toLowerCase(); //we want this to work with any case
     let b = a.slice(0,1).toUpperCase();
@@ -90,64 +72,67 @@ function cleanPrompt(input) {
     return d;
 }
 
-//this function will perform the work of playing the game and then extracting the results to our results div
+//this function will perform the work of playing 1 round of the game and then extracting the results to our divs
 function extract(pChoice) {
+    let s;
     let results = play(pChoice, computerPlay());
     let didIWin = results.pop();
     let response = results.pop();
-    const resultsContainer = document.querySelector('.results')
-    resultsContainer.textContent = `"${response}"`;
+    
+    if(didIWin == 'Win') {
+        s = "You win the round.";
+        scoreP++;
+    }
+    if(didIWin == 'Lose') {
+        s = "You lose the round.";
+        scoreC++;
+    } 
+    if(didIWin == 'Tie') {
+        s = "Tie round.";
+    }
+    if (scoreP <= 5 && scoreC <= 5){
+        resultsContainer.textContent = `${response}`;
+        counterContainer.textContent = `${s} That was round no.${counter}, the score is ${scoreP} - ${scoreC}`;
+    }
+    if (scoreP == 5) {
+        s = `The game is over! You WIN! The score is ${scoreP} - ${scoreC}`;
+        resultsContainer.textContent = 'You win!';
+        counterContainer.textContent = `${s}`;
+    }
+    if (scoreC == 5){
+        s = `The game is over! You LOST. The score is ${scoreP} - ${scoreC}`;
+        resultsContainer.textContent = 'You lose!';
+        counterContainer.textContent = `${s}`;
+    }
+    if (scoreP >= 5 && scoreC >= 5) {
+        resultsContainer.textContent = 'The Game Is Over';
+        counterContainer.textContent = 'Please refresh in order to reset';
+    }
+
 }
+
+//counter variables
+let scoreP = 0;
+let scoreC = 0;
+const counterContainer = document.querySelector('.counter');
+const resultsContainer = document.querySelector('.results');
+let counter = 1;
 
 //Rock button
 const rockb = document.querySelector('.rockB');
-rockb.addEventListener('click', function (){ //must be done with callback function
+rockb.addEventListener('click', function (){
     extract('Rock');
+    counter++;
 });
 //Paper button
 const paperb = document.querySelector('.paperB');
 paperb.addEventListener('click', function (){
     extract('Paper');
+    counter++;
 });
 //Scissors button
 const scissorsb = document.querySelector('.scissorsB');
 scissorsb.addEventListener('click', function (){
     extract('Scissors');
+    counter++;
 });
-
-//play('rock', computerPlay)
-
-//4th function will be a loop that plays the game in multiple rounds.
-// function game() {
-//     let counter = 0;
-//     let winCounter = 0;
-//     let lossCounter = 0;
-//     let tieCounter = 0;
-//     let z;
-// for (let i = 0; i < 5; i++) {
-//     let playerAnswer = prompt();
-//     let cleanPA = cleanPrompt(playerAnswer);
-//     let cleanCA = cleanPrompt(computerPlay());
-//     z = play(cleanPA, cleanCA);
-//     if (z[1] == 'Tie') {
-//         tieCounter++;
-//     }
-//     if (z[1] == 'Loss') {
-//         lossCounter++;
-//     }
-//     if (z[1] == 'Win') {
-//         winCounter++;
-//     }
-//     console.log('Round number: ' + counter);
-//     console.log(z);
-//     counter++;
-// }
-// console.log('There were: ' + winCounter + ' wins; ' + lossCounter + " losses; " + tieCounter + ' ties. Game Over!');
-// return z;
-// }
-// game();
-
-//computerPlay();
-
-//console.log(cleanPA + cleanCA);
-
